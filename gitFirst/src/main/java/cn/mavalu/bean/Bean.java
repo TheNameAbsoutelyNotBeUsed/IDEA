376c7c2a3bdc8cb8798f8d455a194ba8bdb7d9f5
ml <191882566@qq.com>
2019-12-06 16:06:01 +0800

    first commit from IDEA by Malu

---
+++ gitFirst/src/main/java/cn/mavalu/bean/Bean.java
@@ -0,0 +1,10 @@
+package cn.mavalu.bean;
+
+/**
+ * @Author MavaL
+ * @createtime 2019--12--06--15:53
+ */
+public class Bean {
+
+
+}
---
+++ gitFirst/src/test/java/test/gitTest.java
@@ -0,0 +1,9 @@
+package test;
+
+/**
+ * @Author MavaL
+ * @createtime 2019--12--06--15:54
+ */
+public class gitTest {
+
+}
